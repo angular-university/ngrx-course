da17096740b74c3d27295256d3540a3e4fec844c
Your Name <you@example.com>
2019-06-26 16:29:11 +0200

    NgRx - The Complete Guide

---
+++ src/app/auth/reducers/index.ts
@@ -0,0 +1,18 @@
+import {
+  ActionReducer,
+  ActionReducerMap,
+  createFeatureSelector,
+  createSelector,
+  MetaReducer
+} from '@ngrx/store';
+
+
+
+export interface State {
+
+}
+
+export const reducers: ActionReducerMap<State> = {
+
+};
+
