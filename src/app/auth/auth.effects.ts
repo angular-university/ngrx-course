bb886e28781f2e70db7e882c8aa699ea0037517b
Your Name <you@example.com>
2019-07-11 14:48:43 +0200

    NgRx - The Complete Guide

--- src/app/auth/auth.effects.ts
+++ src/app/auth/auth.effects.ts
@@ -1,22 +1,25 @@
 import {Injectable} from '@angular/core';
-import {Actions, createEffect} from '@ngrx/effects';
+import {Actions, createEffect, ofType} from '@ngrx/effects';
+import {AuthActions} from './action-types';
+import {tap} from 'rxjs/operators';
 
 
 @Injectable()
 export class AuthEffects {
 
+    login$ = createEffect(() =>
+        this.actions$
+            .pipe(
+                ofType(AuthActions.login),
+                tap(action => localStorage.setItem('user',
+                        JSON.stringify(action.user))
+                )
+            )
+    ,
+    {dispatch: false});
+
+
     constructor(private actions$: Actions) {
-
-        actions$.subscribe(action => {
-
-            if (action.type == '[Login Page] User Login') {
-
-                localStorage.setItem('user',
-                    JSON.stringify(action["user"]));
-            }
-
-
-        });
 
     }
 
