d000f367a1fb2d4d5faa63c923fbbbde07b774e1
Your Name <you@example.com>
2019-07-15 15:07:14 +0200

    NgRx - The Complete Guide

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -16,6 +16,19 @@ export const reducers: ActionReducerMap<AppState> = {
     router: routerReducer
 };
 
+export function logger(reducer:ActionReducer<any>)
+    : ActionReducer<any> {
+    return (state, action) => {
+        console.log("state before: ", state);
+        console.log("action", action);
+
+        return reducer(state, action);
+    }
+
+}
+
 
 export const metaReducers: MetaReducer<AppState>[] =
-    !environment.production ? [] : [];
+    !environment.production ? [logger] : [];
+
+
