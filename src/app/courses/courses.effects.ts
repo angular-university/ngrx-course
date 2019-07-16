638ac4843daaede4e77377011a30597264d36159
Your Name <you@example.com>
2019-07-16 16:58:31 +0200

    NgRx - The Complete Guide

--- src/app/courses/courses.effects.ts
+++ src/app/courses/courses.effects.ts
@@ -20,6 +20,19 @@ export class CoursesEffects {
             )
     );
 
+
+    saveCourse$ = createEffect(
+        () => this.actions$
+            .pipe(
+                ofType(CourseActions.courseUpdated),
+                concatMap(action => this.coursesHttpService.saveCourse(
+                    action.update.id,
+                    action.update.changes
+                ))
+            ),
+        {dispatch: false}
+    );
+
     constructor(private actions$: Actions,
                 private coursesHttpService: CoursesHttpService) {
 
