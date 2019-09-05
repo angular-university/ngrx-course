4fec0d59b3966be9304871042a661c6295495761
Your Name <you@example.com>
2019-09-05 15:09:43 +0200

    NgRx - The Complete Guide

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -21,66 +21,92 @@ import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {RouterModule, Routes} from '@angular/router';
-import { EntityDataService, EntityDefinitionService, EntityMetadataMap} from '@ngrx/data';
+import {EntityDataService, EntityDefinitionService, EntityMetadataMap} from '@ngrx/data';
 import {compareCourses, Course} from './model/course';
 
 import {compareLessons, Lesson} from './model/lesson';
+import {CourseEntityService} from './services/course-entity.service';
+import {CoursesResolver} from './services/courses.resolver';
+import {CoursesDataService} from './services/courses-data.service';
 
 
 export const coursesRoutes: Routes = [
-  {
-    path: '',
-    component: HomeComponent
-
-  },
-  {
-    path: ':courseUrl',
-    component: CourseComponent
-  }
+    {
+        path: '',
+        component: HomeComponent,
+        resolve: {
+            courses: CoursesResolver
+        }
+    },
+    {
+        path: ':courseUrl',
+        component: CourseComponent,
+        resolve: {
+            courses: CoursesResolver
+        }
+    }
 ];
 
+const entityMetadata: EntityMetadataMap = {
+    Course: {
+        sortComparer: compareCourses
+    }
+};
+
+
 @NgModule({
-  imports: [
-    CommonModule,
-    MatButtonModule,
-    MatIconModule,
-    MatCardModule,
-    MatTabsModule,
-    MatInputModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
-    MatProgressSpinnerModule,
-    MatSlideToggleModule,
-    MatDialogModule,
-    MatSelectModule,
-    MatDatepickerModule,
-    MatMomentDateModule,
-    ReactiveFormsModule,
-    RouterModule.forChild(coursesRoutes)
-  ],
-  declarations: [
-    HomeComponent,
-    CoursesCardListComponent,
-    EditCourseDialogComponent,
-    CourseComponent
-  ],
-  exports: [
-    HomeComponent,
-    CoursesCardListComponent,
-    EditCourseDialogComponent,
-    CourseComponent
-  ],
-  entryComponents: [EditCourseDialogComponent],
-  providers: [
-    CoursesHttpService
-  ]
+    imports: [
+        CommonModule,
+        MatButtonModule,
+        MatIconModule,
+        MatCardModule,
+        MatTabsModule,
+        MatInputModule,
+        MatTableModule,
+        MatPaginatorModule,
+        MatSortModule,
+        MatProgressSpinnerModule,
+        MatSlideToggleModule,
+        MatDialogModule,
+        MatSelectModule,
+        MatDatepickerModule,
+        MatMomentDateModule,
+        ReactiveFormsModule,
+        RouterModule.forChild(coursesRoutes)
+    ],
+    declarations: [
+        HomeComponent,
+        CoursesCardListComponent,
+        EditCourseDialogComponent,
+        CourseComponent
+    ],
+    exports: [
+        HomeComponent,
+        CoursesCardListComponent,
+
+        EditCourseDialogComponent,
+        CourseComponent
+    ],
+    entryComponents: [EditCourseDialogComponent],
+    providers: [
+        CoursesHttpService,
+        CourseEntityService,
+        CoursesResolver,
+        CoursesDataService
+    ]
 })
 export class CoursesModule {
 
-  constructor() {
+    constructor(
+        private eds: EntityDefinitionService,
+        private entityDataService: EntityDataService,
+        private coursesDataService: CoursesDataService) {
 
-  }
+        eds.registerMetadataMap(entityMetadata);
+
+        entityDataService.registerService('Course', coursesDataService);
+
+    }
 
 
 }
--- src/app/courses/home/home.component.ts
+++ src/app/courses/home/home.component.ts
@@ -6,6 +6,7 @@ import {EditCourseDialogComponent} from '../edit-course-dialog/edit-course-dialo
 import {MatDialog} from '@angular/material';
 import {map, shareReplay} from 'rxjs/operators';
 import {CoursesHttpService} from '../services/courses-http.service';
+import {CourseEntityService} from '../services/course-entity.service';
 
 
 
@@ -18,16 +19,13 @@ export class HomeComponent implements OnInit {
 
     promoTotal$: Observable<number>;
 
-    loading$: Observable<boolean>;
-
     beginnerCourses$: Observable<Course[]>;
 
     advancedCourses$: Observable<Course[]>;
 
-
     constructor(
       private dialog: MatDialog,
-      private coursesHttpService: CoursesHttpService) {
+      private coursesService: CourseEntityService) {
 
     }
 
@@ -37,26 +35,17 @@ export class HomeComponent implements OnInit {
 
   reload() {
 
-    const courses$ = this.coursesHttpService.findAllCourses()
-      .pipe(
-        map(courses => courses.sort(compareCourses)),
-        shareReplay()
-      );
-
-    this.loading$ = courses$.pipe(map(courses => !!courses));
-
-    this.beginnerCourses$ = courses$
+    this.beginnerCourses$ = this.coursesService.entities$
       .pipe(
         map(courses => courses.filter(course => course.category == 'BEGINNER'))
       );
 
-
-    this.advancedCourses$ = courses$
+    this.advancedCourses$ = this.coursesService.entities$
       .pipe(
         map(courses => courses.filter(course => course.category == 'ADVANCED'))
       );
 
-    this.promoTotal$ = courses$
+    this.promoTotal$ = this.coursesService.entities$
         .pipe(
             map(courses => courses.filter(course => course.promo).length)
         );
