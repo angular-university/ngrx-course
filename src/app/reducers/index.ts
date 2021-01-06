e57a2f2b1785e122eab93d643b28d9c1f47ac732
CliffDaniel <cliff.clemente@vglsy.com>
2021-01-06 14:43:30 +0100

    lesson 24

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -16,5 +16,16 @@ export const reducers: ActionReducerMap<AppState> = {
   router: routerReducer
 };
 
+export function logger(reducer: ActionReducer<any>): ActionReducer<any>{
 
-export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [] : [];
+  return (state, action) => {
+    console.log("state before: ", state);
+    console.log("action", action);
+
+    return reducer(state, action);
+  }
+
+}
+
+export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [logger] : [];
+
