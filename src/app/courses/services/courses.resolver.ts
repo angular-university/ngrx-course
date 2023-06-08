4a87ffb57834fd763c2ffd74e4a1a7b86c95b1b3
vasco <jhades.dev@gmail.com>
2023-06-08 15:39:56 +0200

    Upgrade to Angular 16

--- src/app/courses/services/courses.resolver.ts
+++ src/app/courses/services/courses.resolver.ts
@@ -1,12 +1,12 @@
 import {Injectable} from '@angular/core';
-import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import {Observable} from 'rxjs';
 import {CourseEntityService} from './course-entity.service';
 import {filter, first, map, tap} from 'rxjs/operators';
 
 
 @Injectable()
-export class CoursesResolver implements Resolve<boolean> {
+export class CoursesResolver  {
 
     constructor(private coursesService: CourseEntityService) {
 
