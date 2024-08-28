4c80ba5f0b68ab6b816dfcfa21f402f3654c3aef
samrids99 <samridsdale108@gmail.com>
2024-08-28 13:47:43 +0100

    saving updated data with an effect

--- src/app/courses/courses.effects.ts
+++ src/app/courses/courses.effects.ts
@@ -13,9 +13,18 @@ export class CoursesEffects {
         () => this.actions$.pipe(
             ofType(CourseActions.loadAllCourses),
             concatMap(() => this.coursesHttpService.findAllCourses()),
-            map(courses => allCoursesLoaded({ courses }))
+            map(courses => allCoursesLoaded({ courses })) // dispatches the allCoursesLoaded action with the courses payload
         )
     )
+
+    saveCourse$ = createEffect(
+        () => this.actions$.pipe(
+            ofType(CourseActions.courseUpdated),
+            concatMap((action) => this.coursesHttpService.saveCourse(action.update.id, action.update.changes))
+        ),
+        { dispatch: false } // we don't want to dispatch any action after saving the course
+
+    );
 
     constructor(private actions$: Actions, private coursesHttpService: CoursesHttpService) {
 
