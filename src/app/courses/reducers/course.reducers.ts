d7277bb27c1a156154912280d18a4f4a429a2113
samrids99 <samridsdale108@gmail.com>
2024-08-28 13:38:41 +0100

    using reducers to save new data in the store

--- src/app/courses/reducers/course.reducers.ts
+++ src/app/courses/reducers/course.reducers.ts
@@ -20,7 +20,8 @@ export const initialCoursesState = adapter.getInitialState({
 
 export const coursesReducer = createReducer(
     initialCoursesState,
-    on(CourseActions.allCoursesLoaded, (state, action) => adapter.addMany(action.courses, {...state, allCoursesLoaded: true}) )
+    on(CourseActions.allCoursesLoaded, (state, action) => adapter.addMany(action.courses, {...state, allCoursesLoaded: true}) ),
+    on(CourseActions.courseUpdated, (state, action) => adapter.updateOne(action.update, state) )
 );
 
 
